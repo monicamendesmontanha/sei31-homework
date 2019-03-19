e966b19fd64b4f3e71ec7a3e8ddbd0d5ee007087
Mônica Mendes <monicamendes@gmail.com>
2019-03-19 17:01:05 +1100

    solution of the two first strings exercise

--- monica_mendes_montanha/week01/the_calculater/strings.js
+++ monica_mendes_montanha/week01/the_calculater/strings.js
@@ -6,30 +6,64 @@ Create a function called DrEvil. It should take a single argument, an amount, an
 
   DrEvil(10): 10 dollars
   DrEvil(1000000): 1000000 dollars (pinky)
+*/
 
-  
+
+function DrElvil(amount) {
+    if (amount < 1000000 ){
+        console.log( `${ amount } 'dollars'`);
+    }else {
+        console.log( `${ amount } 'dollars' (pinky)`);
+    }
+};
+
+DrElvil(90000);
+DrElvil(20000000);
+
+/*
   MixUp
-Create a function called mixUp. It should take in two strings, and return the concatenation of the two strings (separated by a space) slicing out and swapping the first 2 characters of each. You can assume that the strings are at least 2 characters long. For example:
+Create a function called mixUp. It should take in two strings, 
+and return the concatenation of the two strings (separated by a space) slicing out and swapping the first 2 characters of each. 
+You can assume that the strings are at least 2 characters long. For example:
 
   mixUp('mix', 'pod'): 'pox mid'
   mixUp('dog', 'dinner'): 'dig donner'
 Look up the JavaScript string reference to find methods which may be useful!
+*/
+
+//Syntax: string.slice(start, end)
 
 
+function mixUp (string1, string2) {
+    return string1.slice(0,2) + " " + string2.slice(0,2);
+};
+
+console.log(mixUp('mix', 'pod'));
+console.log(mixUp('start', 'end'));
+
+
+/*
 FixStart
 Create a function called fixStart. It should take a single argument, a string, and return a version where all occurences of its first character have been replaced with '*', except for the first character itself. You can assume that the string is at least one character long. For example:
 
 fixStart('babble'): 'ba**le'
+*/
 
 
+
+/*
 Verbing
 Create a function called verbing. It should take a single argument, a string. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. For example:
 
   verbing('swim'): 'swimming'
   verbing('swimming'): 'swimmingly'
   verbing('go'): 'go'
+*/
 
 
+
+
+/*
   Not Bad
 Create a function called notBad that takes a single argument, a string.
 
