dff3bab7d7bb7b745b0f69224ad02606aee8d1ee
Mônica Mendes <monicamendes@gmail.com>
2019-03-19 20:41:58 +1100

    added fixStart function

--- monica_mendes_montanha/week01/the_calculater/strings.js
+++ monica_mendes_montanha/week01/the_calculater/strings.js
@@ -44,16 +44,32 @@ console.log(mixUp('start', 'end'));
 
 /*
 FixStart
-Create a function called fixStart. It should take a single argument, a string, and return a version where all occurences of its first character have been replaced with '*', except for the first character itself. You can assume that the string is at least one character long. For example:
+Create a function called fixStart. It should take a single argument, a string, 
+and return a version where all occurences of its first character have been 
+replaced with '*', except for the first character itself. 
+You can assume that the string is at least one character long. For example:
 
 fixStart('babble'): 'ba**le'
 */
+
+const fixStart = function(string) {
+    const firstChar = string[0];
+    const regex = new RegExp(firstChar, "g");
+    const newString = string.replace(regex, "*"); //string.replace(searchvalue, newvalue)
+    return newString.replace("*", firstChar);
+};
+
+console.log(fixStart("burble"));
+console.log(fixStart("character"));
 
 
 
 /*
 Verbing
-Create a function called verbing. It should take a single argument, a string. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. For example:
+Create a function called verbing. It should take a single argument, a string. 
+If its length is at least 3, it should add 'ing' to its end, 
+unless it already ends in 'ing', in which case it should add 'ly' instead. 
+If the string length is less than 3, it should leave it unchanged. For example:
 
   verbing('swim'): 'swimming'
   verbing('swimming'): 'swimmingly'
@@ -68,9 +84,10 @@ Create a function called verbing. It should take a single argument, a string. If
 Create a function called notBad that takes a single argument, a string.
 
 It should find the first appearance of the substring 'not' and 'bad'.
-If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
-If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
-For example:
+If the 'bad' follows the 'not', then it should replace the 
+whole 'not'...'bad' substring with 'good' and return the result.
+If it doesn't find 'not' and 'bad' in the right sequence (or at all), 
+just return the original sentence. For example:
 
   notBad('This dinner is not that bad!'): 'This dinner is good!'
   notBad('This movie is not so bad!'): 'This movie is good!'
