ef3ca274d2fcf27770c4d205cee8fc34235cac2f
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-30 20:41:04 +1100

    added: make a withdraw and keep track of balance.

--- monica_mendes_montanha/week02/day10/ga-bank/js/atm.js
+++ monica_mendes_montanha/week02/day10/ga-bank/js/atm.js
@@ -4,12 +4,23 @@ console.log('loading...')
 
 $("#checking-deposit").click(function(){
   let currentValue = $("#checking-balance").text().replace("$", "");
-  //console.log(currentValue)
 
   const value = $("#checking-amount").val();
   currentValue = parseInt(currentValue) + parseInt(value);
   $("#checking-balance").text(`$ ${currentValue}`);
-  console.log(currentValue)
 });
+
+
+$("#checking-withdraw").click(function() {
+  let currentValue = $("#checking-balance").text().replace("$", "");
+
+  const value = $("#checking-amount").val();
+  currentValue = parseInt(currentValue) - parseInt(value);
+  $("#checking-balance").text(`$ ${currentValue}`);
+  console.log(currentValue);
+
+});
+
+
 
 });
