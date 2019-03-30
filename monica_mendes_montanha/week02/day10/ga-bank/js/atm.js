db8d364b1b706443d00d2f34155b915814ec5dfd
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-30 22:55:45 +1100

    add overdraft protection from checking account

--- monica_mendes_montanha/week02/day10/ga-bank/js/atm.js
+++ monica_mendes_montanha/week02/day10/ga-bank/js/atm.js
@@ -1,60 +1,86 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //console.log('loading...')
 
-//console.log('loading...')
+  const checkStatusBalance = function(balance) {
+    const currentBalance = $(balance)
+      .text()
+      .replace("$", "");
 
-const checkStatusBalance = function(balance) {
-  const currentBalance = $(balance).text().replace("$", "");
+    if (parseInt(currentBalance) === 0) {
+      $(balance).css("background-color", "red");
+    } else {
+      $(balance).css("background-color", "grey");
+    }
+  };
 
-  if(parseInt(currentBalance) === 0) {
-    $(balance).css("background-color", "red");
-  } else {
-    $(balance).css("background-color", "grey");
-  }
-};
+  $("#checking-deposit").click(function() {
+    let currentBalance = $("#checking-balance")
+      .text()
+      .replace("$", "");
+    const value = $("#checking-amount").val();
 
-$("#checking-deposit").click(function(){
-  let currentBalance = $("#checking-balance").text().replace("$", "");
-  const value = $("#checking-amount").val();
+    currentBalance = parseInt(currentBalance) + parseInt(value);
 
-  currentBalance = parseInt(currentBalance) + parseInt(value);
+    $("#checking-balance").text(`$${currentBalance}`);
+    checkStatusBalance("#checking-balance");
+  });
 
-  $("#checking-balance").text(`$${currentBalance}`);
-  checkStatusBalance("#checking-balance");
-});
+  $("#savings-deposit").click(function() {
+    let currentBalance = $("#savings-balance")
+      .text()
+      .replace("$", "");
+    const value = $("#savings-amount").val();
 
-$("#savings-deposit").click(function(){
-  let currentBalance = $("#savings-balance").text().replace("$", "");
-  const value = $("#savings-amount").val();
+    currentBalance = parseInt(currentBalance) + parseInt(value);
 
-  currentBalance = parseInt(currentBalance) + parseInt(value);
+    $("#savings-balance").text(`$${currentBalance}`);
+    checkStatusBalance("#savings-balance");
+  });
 
-  $("#savings-balance").text(`$${currentBalance}`);
-  checkStatusBalance("#savings-balance");
-});
+  $("#checking-withdraw").click(function() {
+    let currentCheckingBalance = parseInt(
+      $("#checking-balance")
+        .text()
+        .replace("$", "")
+    );
+    let currentSavingsBalance = parseInt(
+      $("#savings-balance")
+        .text()
+        .replace("$", "")
+    );
 
-$("#checking-withdraw").click(function() {
-  let currentBalance = parseInt($("#checking-balance").text().replace("$", ""));
-  const value = parseInt($("#checking-amount").val());
+    const withdrawValue = parseInt($("#checking-amount").val());
 
-  if(value <= currentBalance) {
-    currentBalance = currentBalance - value;;
-  }
+    if (withdrawValue <= currentCheckingBalance) {
+      currentCheckingBalance = currentCheckingBalance - withdrawValue;
+    } else {
+      if (withdrawValue <= currentCheckingBalance + currentSavingsBalance) {
+        let remaining = withdrawValue;
+        remaining = currentCheckingBalance - withdrawValue;
+        currentCheckingBalance = 0;
+        currentSavingsBalance = currentCheckingBalance + currentSavingsBalance - (remaining*-1);
+      }
+    }
 
-  $("#checking-balance").text(`$${currentBalance}`);
-  checkStatusBalance("#checking-balance");
-});
+    $("#checking-balance").text(`$${currentCheckingBalance}`);
+    $("#savings-balance").text(`$${currentSavingsBalance}`);
+    checkStatusBalance("#checking-balance");
+    checkStatusBalance("#savings-balance");
+  });
 
-$("#savings-withdraw").click(function() {
-  let currentBalance = parseInt($("#savings-balance").text().replace("$", ""));
-  const value = parseInt($("#savings-amount").val());
+  $("#savings-withdraw").click(function() {
+    let currentBalance = parseInt(
+      $("#savings-balance")
+        .text()
+        .replace("$", "")
+    );
+    const value = parseInt($("#savings-amount").val());
 
-  if(value <= currentBalance) {
-    currentBalance = currentBalance - value;;
-  }
+    if (value <= currentBalance) {
+      currentBalance = currentBalance - value;
+    }
 
-  $("#savings-balance").text(`$${currentBalance}`);
-  checkStatusBalance("#savings-balance");
-});
-
-
+    $("#savings-balance").text(`$${currentBalance}`);
+    checkStatusBalance("#savings-balance");
+  });
 });
