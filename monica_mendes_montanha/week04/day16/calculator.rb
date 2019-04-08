c772077f43ea5fc3274de1c87e43caed3c0c6987
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-08 16:04:00 +1000

    Solution of caculator

--- monica_mendes_montanha/week04/day16/calculator.rb
+++ monica_mendes_montanha/week04/day16/calculator.rb
@@ -4,10 +4,15 @@ require 'rainbow'
 # binding.pry
 
 def show_menu
-  puts "Calculator" #TODO: you could .center here to make it look nice
-  puts "-=" * 40 # El cheapo divider horinzotal line
+  puts "-=" * 15
+  puts "CALCULATOR" #TODO: you could .center here to make it look nice
+  puts "--" * 15 # El cheapo divider horinzotal line
   puts "[a] - Addition"
-  #TODO" Add other options here
+  puts "[s] - Subtraction"
+  puts "[m] - Multiplication"
+  puts "[d] - Division"
+  puts "[e] - Exponentiation"
+  puts "[sr] - Square_roots"
   puts "[q] - Quit"
 end
 
@@ -15,12 +20,26 @@ show_menu
 menu_choice = gets.chomp.downcase
 
 until menu_choice == 'q'
+
+  puts "Enter the number: "
+  a = gets.to_f
+  puts "Enter the number: "
+  b = gets.to_f
+  puts "-•" *15
+
   case menu_choice
   when 'a'
-    # TODO: get number a
-    # TODO: get number begin
-    # TODO: call add a, b
-    puts "Addition coming soon"
+    p "#{a} + #{b} = #{a+b}"
+  when 's'
+    p "#{a} - #{b} = #{a-b}"
+  when 'm'
+    p "#{a} * #{b} = #{a*b}"
+  when 'd'
+    p "#{a} / #{b} = #{a/b}"
+  when 'e'
+    p "#{a} ** #{b} = #{a**b}"
+  when 'sr'
+    p "#{Integer.sqrt(a)}"
   else
     puts "Invalid selection!"
   end
