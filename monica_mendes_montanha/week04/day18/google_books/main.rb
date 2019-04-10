1690f6c7a3618f0778defcd5ce0fb3a7b2aefc9a
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-10 19:00:47 +1000

    The solution of books exercise using the httparty

---
+++ monica_mendes_montanha/week04/day18/google_books/main.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
+
+get '/' do
+  erb :home
+end
+
+get '/result' do
+  @title = params[:title]
+
+  url = "https://www.googleapis.com/books/v1/volumes?q=title:#{@title}"
+  @info = HTTParty.get url
+
+  @book_cover = @info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+
+  erb :result
+
+ end
