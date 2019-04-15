e765325ba727bcc5557e7b316e11434496d24925
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-15 22:32:55 +1000

    Solution of secret number

--- monica_mendes_montanha/week05/day21/games/app/controllers/secret_number_controller.rb
+++ monica_mendes_montanha/week05/day21/games/app/controllers/secret_number_controller.rb
@@ -1,4 +1,17 @@
 class SecretNumberController < ApplicationController
   def index
   end
+
+  def guess
+    @guessed_number = params[:guessed_number].to_i
+    @secret_number = rand(1..10)
+
+    if @guessed_number == @secret_number
+      @message = "You won!"
+    else
+      @message = "You lose! Try again!"
+    end
+
+    render :index
+  end
 end
