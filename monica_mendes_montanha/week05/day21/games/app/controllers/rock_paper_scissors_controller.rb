2ca58a832fcec7effa396c379f75055c461cab73
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-15 23:15:27 +1000

    Refactor game rules

--- monica_mendes_montanha/week05/day21/games/app/controllers/rock_paper_scissors_controller.rb
+++ monica_mendes_montanha/week05/day21/games/app/controllers/rock_paper_scissors_controller.rb
@@ -6,36 +6,40 @@ class RockPaperScissorsController < ApplicationController
     @player_1 = params[:throw]
     @player_2 = computer
 
-    if @player_1 == @player_2
-      @message = "It's a draw"
-    end
-
-    if @player_1 == 'rock' && @player_2 == 'scissors'
-      @message = "#{@player_1} player wins!"
-    end
-
-    if @player_1 == 'rock' && @player_2 == 'paper'
-      @message = "#{@player_2} player wins!"
-    end
-
-    if @player_1 == 'paper' && @player_2 == 'rock'
-      @message = "#{@player_1} player wins!"
-    end
-
-    if @player_1 == 'paper' && @player_2 == 'scissors'
-      @message = "#{@player_2} player wins!"
-    end
-
-    if @player_1 == 'scissors' && @player_2 == 'rock'
-      @message = "#{@player_2} player wins!"
-    end
-
-    if @player_1 == 'scissors' && @player_2 == 'paper'
-      @message = "#{@player_1} player wins!"
-    end
+    @message = game_rules(@player_1, @player_2)
   end
 
   private
+
+  def game_rules(player_1, player_2)
+    if player_1 == player_2
+      return "It's a draw"
+    end
+
+    if player_1 == 'rock' && player_2 == 'scissors'
+      return "#{player_1} player wins!"
+    end
+
+    if player_1 == 'rock' && player_2 == 'paper'
+      return "#{player_2} player wins!"
+    end
+
+    if player_1 == 'paper' && player_2 == 'rock'
+      return "#{player_1} player wins!"
+    end
+
+    if player_1 == 'paper' && player_2 == 'scissors'
+      return "#{player_2} player wins!"
+    end
+
+    if player_1 == 'scissors' && player_2 == 'rock'
+      return "#{player_2} player wins!"
+    end
+
+    if player_1 == 'scissors' && player_2 == 'paper'
+      return "#{player_1} player wins!"
+    end
+  end
 
   def computer
     ['rock', 'paper', 'scissors'].sample
