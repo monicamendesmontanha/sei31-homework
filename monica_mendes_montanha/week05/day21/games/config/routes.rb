ad36c1fba9298044a90845953642d3e33e1ac73d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-15 22:32:43 +1000

    Change route redirect for magic8_ball

--- monica_mendes_montanha/week05/day21/games/config/routes.rb
+++ monica_mendes_montanha/week05/day21/games/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
+
   get '/magic8_ball' => 'magic8_ball#index'
-  post '/magic8_ball/answer' => 'magic8_ball#answer'
+  post '/magic8_ball' => 'magic8_ball#answer'
+
   get '/secret_number' => 'secret_number#index'
+  post '/secret_number' => 'secret_number#guess'
+
   get '/rock_paper_scissors' => 'rock_paper_scissors#index'
 end
