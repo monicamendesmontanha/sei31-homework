7471f173a111700960189410ccc7b40ecfb5cf34
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-16 17:12:41 +1000

    Add form and the pages to create and to edit

--- monica_mendes_montanha/week05/day22/mountains/app/controllers/mountains_controller.rb
+++ monica_mendes_montanha/week05/day22/mountains/app/controllers/mountains_controller.rb
@@ -4,4 +4,35 @@ class MountainsController < ApplicationController
     @mountains = Mountain.all
   end
 
+  def show
+    @mountain = Mountain.find params[:id]
+  end
+
+  def new
+    @mountain = Mountain.new
+  end
+
+  def create
+    mountain = Mountain.create mountain_params
+    redirect_to mountain_path(mountain.id)
+  end
+
+  def edit
+    @mountain = Mountain.find params[:id]
+  end
+
+  def update
+    mountain = Mountain.find params[:id]
+    mountain.update mountain_params
+    redirect_to mountain_path(mountain.id)
+  end
+
+  private
+  def mountain_params
+    # Strong parameters: whitelist of sanitized input (stuff that is OKAY to put in the database)
+    params.require(:mountain).permit(:name, :country, :city, :image)
+  end
+
+
+
 end
